e287faa42dacb8e37219d799294c1482afa3e1bc
Rahul Chaurasia <rahulrchaurasia@gmail.com>
2024-06-10 15:50:52 +0530

    update OTP Mobile Type

---
+++ policyBoss/mainVC/SALESMATERIAL/model/SalesMaterialContentUsageRequest.swift
@@ -0,0 +1,9 @@
+//
+//  SalesMaterialContentUsageRequest.swift
+//  policyBoss
+//
+//  Created by Rahul Chaurasia on 14/03/24.
+//  Copyright © 2024 policyBoss. All rights reserved.
+//
+
+import Foundation
